a0ed4a103ce5a0fc8e265cdcec22f7a14e868e37
Acheniche <ache.niche00@mail.ru>
2024-01-25 14:52:27 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import Zlib from 'zlib';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const inputStream = fs.createReadStream(path.join(__dirname, './files/fileToCompress.txt'));
+    const outputStream = fs.createWriteStream(path.join(__dirname, './files/archive.gz'));
+    inputStream.pipe(Zlib.createGzip()).pipe(outputStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import Zlib from 'zlib';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    const inputStream = fs.createReadStream(path.join(__dirname, './files/archive.gz'));
+    const outputStream = fs.createWriteStream(path.join(__dirname, './files/fileToCompress.txt'));
+    inputStream.pipe(Zlib.createUnzip()).pipe(outputStream);
 };
 
 await decompress();
